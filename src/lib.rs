f7d57191cefe0008bfcfa01f2ca3a849089069f9
Tony Watkins <tony@llifion.net>
2025-01-25 13:19:49 +0000

    feature: working implementation of singly linked list (may not be that performant)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,43 @@
+#[derive(Debug)]
 pub struct LinkedList {
     head: Link,
 }
 
 impl LinkedList {
     pub fn empty() -> LinkedList {
-        LinkedList { head: None }
+        Self { head: None }
+    }
+
+    pub fn push(&mut self, element: u32) {
+        // We can't use match on the Some case, as this tries to move data out of the list
+        // this isn't allowed as data is only borrowed not owned -
+        // std::mem replace functions comes into play
+        let old_head = std::mem::replace(&mut self.head, None);
+        let new_head = Box::new(Node {
+            element,
+            next: old_head,
+        });
+        self.head = Some(new_head);
+
+        // match self.head {
+        //     None => {
+        //         self.head = Some(Box::new(Node {
+        //             element,
+        //             next: None,
+        //         }))
+        //     }
+        //     Some(n) => {
+        //         let new_head = Some(Box::new(Node {
+        //             element,
+        //             next: Some(n),
+        //         }));
+        //         self.head = new_head;
+        //     }
+        // }
     }
 }
 
+#[derive(Debug)]
 struct Node {
     element: u32,
     next: Link,
@@ -21,7 +51,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let list = LinkedList::empty();
+        let mut list = LinkedList::empty();
+        list.push(1024);
+        list.push(42);
+        dbg!(list);
     }
 }
 
